370559c3c1863dc4cca790836d246a4f4cf484d4
Semen Pavelko <se.pa.94@mail.ru>
2021-12-09 00:49:19 +0300

    Task 21 added version 2

--- 21. car-showroom/src/ru/netology/dealership/Seller.java
+++ 21. car-showroom/src/ru/netology/dealership/Seller.java
@@ -2,30 +2,42 @@ package ru.netology.dealership;
 
 import ru.netology.car.Car;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Seller {
     private int index = 0;
     private CarDealership dealership;
+    private final Lock locker = new ReentrantLock();
+    private final Condition availabilityСar = locker.newCondition();
 
     public Seller(CarDealership dealership) {
         this.dealership = dealership;
     }
-    public synchronized Car sellСar() {
+    public Car sellСar() {
+        locker.lock();
         try {
             System.out.printf("%s пришел покупать автомобиль\n", Thread.currentThread().getName());
             while (dealership.isParkingEmpty()) {
                 System.out.println("Машин нет");
-                wait();
+                availabilityСar.await();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            locker.unlock();
         }
         
         return dealership.getParking().remove(index);
     }
 
-    public synchronized void checkCar(Car car) {
+
+    public void checkCar(Car car) {
+        locker.lock();
         dealership.getParking().add(car);
-        notify();
+        availabilityСar.signal();
+        locker.unlock();
     }
 
 
